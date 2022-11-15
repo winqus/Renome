45d34abbca2d9d47820db5f6c4e8f31d6fe6074b
winqus <67459222+winqus@users.noreply.github.com>
2022-11-15 18:37:48 +0200

    Added JS for navigation menus (main and sub).
    The implementation is a bit overcomplicated for only a menu and a sub-menu. But it was indented for learning purposes, utilizing objects. And it is quite flexible as it makes adding more sub-menus (even within sub-menus) much simpler.

--- assets/public/js/main.js
+++ assets/public/js/main.js
@@ -1,3 +1,5 @@
+/* Cart dropdown menu */
+
 const cartBtn = document.getElementById("cartBtn");
 const cartDropdown = document.getElementById("cartDropdown");
 
@@ -6,3 +8,93 @@ function toggleCartDropdown() {
 }
 
 cartBtn.onclick = toggleCartDropdown;
+
+/* Navigation menus */
+
+const mainNavButtonId = "navBtn";
+const featuresSubNavButtonId = "featuresBtn";
+const backButtonId = "backBtn";
+
+// Constructor for NavMenu object.
+function NavMenu(element, showClasses, hideClasses) {
+  this.element = element;
+  this.showClasses = showClasses.split(" ");
+  this.hideClasses = hideClasses.split(" ");
+
+  this.Show = () => {
+    this.hideClasses.forEach(c => this.element.classList.toggle(c, false));
+    this.showClasses.forEach(c => this.element.classList.toggle(c, true));
+  };
+
+  this.Hide = () => {
+    this.showClasses.forEach(c => this.element.classList.toggle(c, false));
+    this.hideClasses.forEach(c => this.element.classList.toggle(c, true));
+  };
+
+  this.IsOpen = () => {
+    let isOpen = true;
+
+    this.showClasses.forEach(c => {
+      if(this.element.classList.contains(c) == false) {
+        isOpen = false;
+      }
+    });
+
+    this.hideClasses.forEach(c => {
+      if(this.element.classList.contains(c) == true) {
+        isOpen = false;
+      }
+    });
+
+    return isOpen;
+  };
+
+  // Default behaviour.
+  this.Hide();
+}
+
+// Constructor for NavButton object.
+function NavButton(element, activeStateClasses, navManager, targetMenu) {
+  this.element = element;
+  this.activeStateClasses = activeStateClasses.split(" ");
+  this.targetMenu = targetMenu;
+
+  this.Toggle = () => {
+    if(targetMenu.IsOpen()) {
+      navManager.HideNavMenusOrExceptOne();
+    } else {
+      if(this.element.id != mainNavButtonId || navManager.IsAnyNavOpen() == false)
+      {
+        targetMenu.Show();
+      }
+      navManager.HideNavMenusOrExceptOne(targetMenu);
+    }
+  };
+
+  // Default behaviour.
+  this.element.onclick = this.Toggle;
+}
+
+// Constructor for NavManager object.
+function NavManager(menuList, mainNavMenu) {
+  this.menus = menuList;
+  this.mainNav = mainNavMenu;
+
+  this.HideNavMenusOrExceptOne = (exceptionMenu = null) => {
+    this.menus.forEach(menu => {
+      if(menu != exceptionMenu) {
+        menu.Hide();
+      }
+    });
+  };
+
+  this.IsAnyNavOpen = () => this.menus.some(menu => menu.IsOpen());
+}
+
+// Objects for nav menu managment.
+const navMenu = new NavMenu(document.getElementById("navMenu"), "nav-menu__list--show-vertical", "nav-menu__list--hide-vertical");
+const featuresSideNavMenu = new NavMenu(document.getElementById("featuresSubMenu"), "nav-menu__list--show-horizontal",  "nav-menu__list--hide-horizontal");
+const navManager = new NavManager([navMenu, featuresSideNavMenu], navMenu);
+const navBtn = new NavButton(document.getElementById(mainNavButtonId), "", navManager, navMenu);
+const featuresSubNavBtn = new NavButton(document.getElementById(featuresSubNavButtonId), "", navManager, featuresSideNavMenu);
+const backBtn = new NavButton(document.getElementById(backButtonId), "", navManager, navMenu);
