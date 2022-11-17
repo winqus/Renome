84e6567159ff8e750fa7031a7f521bc56c65cbf5
winqus <67459222+winqus@users.noreply.github.com>
2022-11-17 14:53:38 +0200

    Updated main.js
    Moved constants to top, replaced once used constants with value, removed comments, simplified some functions.

--- assets/public/js/main.js
+++ assets/public/js/main.js
@@ -1,7 +1,32 @@
-/* Cart dropdown menu */
-
 const cartBtn = document.getElementById("cartBtn");
 const cartDropdown = document.getElementById("cartDropdown");
+const mainNavButtonId = "navBtn";
+const navMenu = new NavMenu(
+  document.getElementById("navMenu"),
+  "nav-menu__list--show-vertical",
+  "nav-menu__list--hide-vertical"
+);
+const featuresSideNavMenu = new NavMenu(
+  document.getElementById("featuresSubMenu"),
+  "nav-menu__list--show-horizontal",
+  "nav-menu__list--hide-horizontal"
+);
+const navManager = new NavManager([navMenu, featuresSideNavMenu], navMenu);
+const navBtn = new NavButton(
+  document.getElementById(mainNavButtonId),
+  navManager,
+  navMenu
+);
+const featuresSubNavBtn = new NavButton(
+  document.getElementById("featuresBtn"),
+  navManager,
+  featuresSideNavMenu
+);
+const backBtn = new NavButton(
+  document.getElementById("backBtn"),
+  navManager,
+  navMenu
+);
 
 function toggleCartDropdown() {
   cartDropdown.classList.toggle("nav-menu__cart-dropdown-content--show");
@@ -9,92 +34,53 @@ function toggleCartDropdown() {
 
 cartBtn.onclick = toggleCartDropdown;
 
-/* Navigation menus */
-
-const mainNavButtonId = "navBtn";
-const featuresSubNavButtonId = "featuresBtn";
-const backButtonId = "backBtn";
-
-// Constructor for NavMenu object.
-function NavMenu(element, showClasses, hideClasses) {
+function NavMenu(element, showClass, hideClass) {
   this.element = element;
-  this.showClasses = showClasses.split(" ");
-  this.hideClasses = hideClasses.split(" ");
+  this.showClasses = showClass;
+  this.hideClasses = hideClass;
 
-  this.Show = () => {
-    this.hideClasses.forEach(c => this.element.classList.toggle(c, false));
-    this.showClasses.forEach(c => this.element.classList.toggle(c, true));
-  };
+  this.ShowNavigation = () => this.element.classList.replace(hideClass, showClass);
 
-  this.Hide = () => {
-    this.showClasses.forEach(c => this.element.classList.toggle(c, false));
-    this.hideClasses.forEach(c => this.element.classList.toggle(c, true));
-  };
+  this.HideNavigation = () => this.element.classList.replace(showClass, hideClass);
 
-  this.IsOpen = () => {
-    let isOpen = true;
+  this.NavigationIsOpen = () => this.element.classList.contains(showClass);
 
-    this.showClasses.forEach(c => {
-      if(this.element.classList.contains(c) == false) {
-        isOpen = false;
-      }
-    });
-
-    this.hideClasses.forEach(c => {
-      if(this.element.classList.contains(c) == true) {
-        isOpen = false;
-      }
-    });
-
-    return isOpen;
-  };
-
-  // Default behaviour.
-  this.Hide();
+  this.HideNavigation();
 }
 
-// Constructor for NavButton object.
 function NavButton(element, navManager, targetMenu) {
   this.element = element;
   this.navManager = navManager;
   this.targetMenu = targetMenu;
 
   this.Toggle = () => {
-    if(this.targetMenu.IsOpen()) {
+    if (this.targetMenu.NavigationIsOpen()) {
       this.navManager.HideNavMenusOrExceptOne();
     } else {
-      if(this.element.id != mainNavButtonId || this.navManager.IsAnyNavOpen() == false)
-      {
-        this.targetMenu.Show();
+      if (
+        this.element.id != mainNavButtonId ||
+        this.navManager.IsAnyNavOpen() == false
+      ) {
+        this.targetMenu.ShowNavigation();
       }
       this.navManager.HideNavMenusOrExceptOne(targetMenu);
     }
   };
 
-  // Default behaviour.
   this.element.onclick = this.Toggle;
 }
 
-// Constructor for NavManager object.
 function NavManager(menuList, mainNavMenu) {
   this.menus = menuList;
   this.mainNav = mainNavMenu;
 
   this.HideNavMenusOrExceptOne = (exceptionMenu = null) => {
-    this.menus.forEach(menu => {
-      if(menu != exceptionMenu) {
-        menu.Hide();
+    this.menus.forEach((menu) => {
+      if (menu != exceptionMenu) {
+        menu.HideNavigation();
       }
     });
   };
 
-  this.IsAnyNavOpen = () => this.menus.some(menu => menu.IsOpen());
+  this.IsAnyNavOpen = () => this.menus.some((menu) => menu.NavigationIsOpen());
 }
-
-// Objects for nav menu managment.
-const navMenu = new NavMenu(document.getElementById("navMenu"), "nav-menu__list--show-vertical", "nav-menu__list--hide-vertical");
-const featuresSideNavMenu = new NavMenu(document.getElementById("featuresSubMenu"), "nav-menu__list--show-horizontal",  "nav-menu__list--hide-horizontal");
-const navManager = new NavManager([navMenu, featuresSideNavMenu], navMenu);
-const navBtn = new NavButton(document.getElementById(mainNavButtonId), navManager, navMenu);
-const featuresSubNavBtn = new NavButton(document.getElementById(featuresSubNavButtonId), navManager, featuresSideNavMenu);
-const backBtn = new NavButton(document.getElementById(backButtonId), navManager, navMenu);
