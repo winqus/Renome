10d342ad0410c49be4f3baadce036094f2bf19f7
winqus <67459222+winqus@users.noreply.github.com>
2022-11-21 15:51:10 +0200

    Added JS to main.js for carousel functionality.

--- assets/public/js/main.js
+++ assets/public/js/main.js
@@ -1,56 +1,133 @@
-const cartBtn = document.getElementById("cartBtn");
-const cartDropdown = document.getElementById("cartDropdown");
-const mainNavMenu = document.getElementById("navMenu");
-const featureSubNavMenu = document.getElementById("featuresSubMenu");
-const mainNavMenuButton = document.getElementById("navBtn");
-const navHambugerIcon = document.getElementById("navHambugerIcon");
+const cartBtn = document.getElementById('cartBtn');
+const cartDropdown = document.getElementById('cartDropdown');
+const mainNavMenu = document.getElementById('navMenu');
+const featureSubNavMenu = document.getElementById('featuresSubMenu');
+const mainNavMenuButton = document.getElementById('navBtn');
+const navHambugerIcon = document.getElementById('navHambugerIcon');
+const slideTrack = document.getElementById('carouselTrack');
+const slides = Array.from(slideTrack.querySelectorAll('.carousel__slide'));
+const carouselSlidingTimout = 400;
+let slidePositionIndex = 0;
+let isCarouselSlidable = true;
+let isLastSlideMoveLeft = false;
+
+const getNonNegativeArrayIndex = (index, arraySize) => (
+  ((index % arraySize) + arraySize) % arraySize
+);
+const getCurrentSlide = () => slides[getNonNegativeArrayIndex(slidePositionIndex, slides.length)];
+const getCarouselImageHeight = () => slides[0].querySelector('.carousel__image').height;
+const getCarouselImageWidth = () => slides[0].querySelector('.carousel__image').width;
+
+const setCarouselHeightToSlideImageHeight = () => {
+  document.getElementById('carousel').style.height = `${getCarouselImageHeight()}px`;
+};
+const setSlidePosition = (slide, index) => {
+  slide.style.left = `${getCarouselImageWidth() * index}px`;
+};
+const moveToSlide = (slide) => {
+  slideTrack.style.transform = `translateX(-${slide.style.left})`.replace('--', '');
+};
+
+const instantMoveToSlide = (slide) => {
+  slideTrack.classList.remove('carousel__slide-track--sliding');
+  moveToSlide(slide);
+  setTimeout(() => { slideTrack.classList.add('carousel__slide-track--sliding'); }, 10);
+};
+const positionSlidesFromSlideIndex = () => {
+  slides.forEach((_, index) => {
+    setSlidePosition(
+      slides[getNonNegativeArrayIndex(slidePositionIndex + index, slides.length)],
+      slidePositionIndex + index,
+    );
+  });
+};
+const moveSlidesLeft = () => {
+  if ((isCarouselSlidable || !isLastSlideMoveLeft) && slides.length > 1) {
+    slidePositionIndex += 1;
+    setSlidePosition(getCurrentSlide(), slidePositionIndex);
+    moveToSlide(getCurrentSlide());
+    isLastSlideMoveLeft = true;
+  }
+};
+const moveSlidesRight = () => {
+  if ((isCarouselSlidable || isLastSlideMoveLeft) && slides.length > 1) {
+    slidePositionIndex -= 1;
+    setSlidePosition(getCurrentSlide(), slidePositionIndex);
+    moveToSlide(getCurrentSlide());
+    isLastSlideMoveLeft = false;
+  }
+};
+const startCarouselTimeout = (milliseconds) => {
+  if (isCarouselSlidable) {
+    isCarouselSlidable = false;
+    setTimeout(() => { isCarouselSlidable = true; }, milliseconds);
+  }
+};
 
 function toggleCartDropdown() {
-  cartDropdown.classList.toggle("nav-menu__cart-dropdown-content--show");
+  cartDropdown.classList.toggle('nav-menu__cart-dropdown-content--show');
 }
 
 cartBtn.onclick = toggleCartDropdown;
 
 mainNavMenuButton.onclick = () => {
-  if (mainNavMenuButton.classList.contains("nav-menu__button--active")) {
-    mainNavMenuButton.classList.remove("nav-menu__button--active");
-    navHambugerIcon.classList.remove("icon-hamburger--active");
+  if (mainNavMenuButton.classList.contains('nav-menu__button--active')) {
+    mainNavMenuButton.classList.remove('nav-menu__button--active');
+    navHambugerIcon.classList.remove('icon-hamburger--active');
     mainNavMenu.classList.replace(
-      "nav-menu__list--show-vertical",
-      "nav-menu__list--hide-vertical"
+      'nav-menu__list--show-vertical',
+      'nav-menu__list--hide-vertical',
     );
     featureSubNavMenu.classList.replace(
-      "nav-menu__list--show-horizontal",
-      "nav-menu__list--hide-horizontal"
+      'nav-menu__list--show-horizontal',
+      'nav-menu__list--hide-horizontal',
     );
   } else {
-    mainNavMenuButton.classList.add("nav-menu__button--active");
+    mainNavMenuButton.classList.add('nav-menu__button--active');
     mainNavMenu.classList.replace(
-      "nav-menu__list--hide-vertical",
-      "nav-menu__list--show-vertical"
+      'nav-menu__list--hide-vertical',
+      'nav-menu__list--show-vertical',
     );
-    navHambugerIcon.classList.add("icon-hamburger--active");
+    navHambugerIcon.classList.add('icon-hamburger--active');
   }
 };
 
-document.getElementById("featuresBtn").onclick = () => {
+document.getElementById('featuresBtn').onclick = () => {
   mainNavMenu.classList.replace(
-    "nav-menu__list--show-vertical",
-    "nav-menu__list--hide-vertical"
+    'nav-menu__list--show-vertical',
+    'nav-menu__list--hide-vertical',
   );
   featureSubNavMenu.classList.replace(
-    "nav-menu__list--hide-horizontal",
-    "nav-menu__list--show-horizontal"
+    'nav-menu__list--hide-horizontal',
+    'nav-menu__list--show-horizontal',
   );
 };
 
-document.getElementById("backBtn").onclick = () => {
+document.getElementById('backBtn').onclick = () => {
   mainNavMenu.classList.replace(
-    "nav-menu__list--hide-vertical",
-    "nav-menu__list--show-vertical"
+    'nav-menu__list--hide-vertical',
+    'nav-menu__list--show-vertical',
   );
   featureSubNavMenu.classList.replace(
-    "nav-menu__list--show-horizontal",
-    "nav-menu__list--hide-horizontal"
+    'nav-menu__list--show-horizontal',
+    'nav-menu__list--hide-horizontal',
   );
 };
+
+document.getElementById('carouselLeftBtn').onclick = () => {
+  moveSlidesRight();
+  startCarouselTimeout(carouselSlidingTimout);
+};
+document.getElementById('carouselRightBtn').onclick = () => {
+  moveSlidesLeft();
+  startCarouselTimeout(carouselSlidingTimout);
+};
+
+window.onresize = () => {
+  setCarouselHeightToSlideImageHeight();
+  positionSlidesFromSlideIndex(slidePositionIndex);
+  instantMoveToSlide(getCurrentSlide());
+};
+
+positionSlidesFromSlideIndex();
+setCarouselHeightToSlideImageHeight();
